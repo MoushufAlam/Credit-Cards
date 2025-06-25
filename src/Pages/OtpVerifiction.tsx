ffe8aeddb874970add46c55d5f22779549565f36
MoushufAlam <rishu78698@gmail.com>
2025-06-25 20:57:44 +0530

    this ts na

--- src/Pages/OtpVerifiction.tsx
+++ src/Pages/OtpVerifiction.tsx
@@ -19,6 +19,30 @@ function OtpVerifiction() {
   const [attemptError, setAttemptError] = useState<boolean>(true)
   const [showOtp, setShowOtp] = useState<boolean>(false)
 
+
+  // const handleOtpVerify = async () => {
+  //   if (otp.length !== 6 || !isValidOtp(otp)) {
+  //     setIsValid(false)
+  //     return
+  //   }
+
+  //   try {
+  //     const response = await axios.post('', {
+  //       phone: phoneNumber,
+  //       otp: otp
+  //     })
+  //     console.log('OTP Verified:', response.data)
+  //     navigate('/profile')
+  //   } catch (error) {
+  //     console.error('OTP validation failed:', error)
+  //     setAttemptError(false)
+  //     setFailedAttempts(prev => prev + 1)
+  //   }
+  // }
+  // console.log(handleOtpVerify);
+  
+  console.log(setFailedAttempts);
+
   useEffect(() => {
     if (timer > 0) {
       const interval = setInterval(() => {
