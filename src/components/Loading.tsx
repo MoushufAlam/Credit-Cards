a045408550e31472f06b54a6bc06204ff8f04a87
MoushufAlam <rishu78698@gmail.com>
2025-06-27 12:16:08 +0530

    Lazy Loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,8 @@
+
+export default function Loading() {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <div className="spinner-border text-primary" role="status" />
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,27 +3,32 @@ import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Signup from './Pages/Signup.tsx'
 import Layout from './components/Layout.tsx'
-import OtpVerifiction from './Pages/OtpVerifiction.tsx'
-import Comparison from './Pages/Comparison.tsx'
-import Profile from './Pages/Profile.tsx'
 import { Provider } from 'react-redux'
 import store from './store.ts'
 import ProtectedRoute from './utils/ProtectedRoute.tsx'
+import { Suspense, lazy } from 'react'
+import Loading from './components/Loading.tsx'
+
+const Signup = lazy(() => import('./Pages/Signup.tsx'))
+const OtpVerifiction = lazy(() => import('./Pages/OtpVerifiction.tsx'))
+const Comparison = lazy(() => import('./Pages/Comparison.tsx'))
+const Profile = lazy(() => import('./Pages/Profile.tsx'))
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="signup" element={<ProtectedRoute><Signup /></ProtectedRoute>} />
-          <Route path="otp-verification" element={<ProtectedRoute><OtpVerifiction /></ProtectedRoute>} />
-          <Route path="compare" element={<ProtectedRoute><Comparison /></ProtectedRoute>} />
-          <Route path="profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<App />} />
+            <Route path="signup" element={<ProtectedRoute><Signup /></ProtectedRoute>} />
+            <Route path="otp-verification" element={<ProtectedRoute><OtpVerifiction /></ProtectedRoute>} />
+            <Route path="compare" element={<ProtectedRoute><Comparison /></ProtectedRoute>} />
+            <Route path="profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </Provider>
 )
